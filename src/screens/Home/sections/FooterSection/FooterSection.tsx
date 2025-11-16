63aaae233415d8de3a91e9808742c9eff827ff7c
Razik <razik3407@gmail.com>
2025-11-16 23:26:33 +0530

    changed

--- src/screens/Home/sections/FooterSection/FooterSection.tsx
+++ src/screens/Home/sections/FooterSection/FooterSection.tsx
@@ -1,5 +1,5 @@
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
+import { Button } from "../../../../components/ui/button";
+import { Input } from "../../../../components/ui/input";
 import React from "react";
 import { FaFacebook, FaInstagram, FaLinkedin, FaYoutube } from "react-icons/fa";
 
